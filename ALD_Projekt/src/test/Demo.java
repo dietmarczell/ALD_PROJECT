d8cf18db9ba8a513cdb1637230f069321116473c
czell <czell@UCL08248.knapp.at>
2016-06-18 12:56:11 +0200

    first commit

---
+++ ALD_Projekt/src/test/Demo.java
@@ -0,0 +1,14 @@
+package test;
+
+public class Demo {
+
+	public static void main(String[] args) throws Exception {
+		// TODO Auto-generated method stub
+		BreitensucheTest bstest = new BreitensucheTest();
+		bstest.setUp();
+		bstest.getBreadthFirstOrder1();
+		bstest.getBreadthFirstOrder2();
+		bstest.getBreadthFirstOrder3();
+	}
+
+}
